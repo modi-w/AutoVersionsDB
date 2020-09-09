32f4e649fce54e60bba61f59af3e16fb0fc44434
modi-w <63314355+modi-w@users.noreply.github.com>
2020-09-09 22:25:58 +0300

    Create Console Project

---
+++ src/AutoVersionsDB/AutoVersionsDB.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoVersionsDB.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
