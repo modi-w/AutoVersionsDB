599022aba0b53a951224bceba2d83d27b5a3c1b3
modi-w <63314355+modi-w@users.noreply.github.com>
2020-10-03 22:56:40 +0300

    Tests -> DeliveryEnv_NotAollowMethods

--- src/AutoVersionsDB/AutoVersionsDB.Core.IntegrationTests/AutoVersionsDbAPI_Tests/AutoVersionsDbAPI_DeliveryEnv_NotAollowMethods_Tests.cs
+++ src/AutoVersionsDB/AutoVersionsDB.Core.IntegrationTests/AutoVersionsDbAPI_Tests/AutoVersionsDbAPI_DeliveryEnv_NotAollowMethods_Tests.cs
@@ -1,80 +1,80 @@
-﻿using AutoVersionsDB.Helpers;
-using AutoVersionsDB.Core.IntegrationTests.AutoVersionsDbAPI_Tests.ProjectConfigItemForTests;
-using AutoVersionsDB.NotificationableEngine;
-using Moq;
-using NUnit.Framework;
-using System.IO;
+﻿//using AutoVersionsDB.Helpers;
+//using AutoVersionsDB.Core.IntegrationTests.AutoVersionsDbAPI_Tests.ProjectConfigItemForTests;
+//using AutoVersionsDB.NotificationableEngine;
+//using Moq;
+//using NUnit.Framework;
+//using System.IO;
 
-namespace AutoVersionsDB.Core.IntegrationTests.AutoVersionsDbAPI_Tests
-{
-    [TestFixture]
-    public class AutoVersionsDbAPI_DeliveryEnv_NotAollowMethods_Tests : AutoVersionsDbAPI_TestsBase
-    {
-
-
-        [Test]
-        public void SetDBToSpecificState__Should_NotAllow([ValueSource("ProjectConfigItemArray_DeliveryEnv_ValidScripts")] ProjectConfigItemForTestBase projectConfig)
-        {
-            //Arrange
-            _mockProjectConfigsStorage.Setup(m => m.GetProjectConfigById(It.IsAny<string>())).Returns(projectConfig);
-
-            string dbBackupFileFileFullPath = Path.Combine(FileSystemPathUtils.GetDllFolderFullPath(), "DbBackupsForTests", "AutoVersionsDB_MiddleState__incScript_2020-02-25.102_CreateLookupTable2.bak");
-            restoreDB(projectConfig, dbBackupFileFileFullPath);
-
-            NumOfConnections numOfOpenConnections_Before = getNumOfOpenConnection(projectConfig);
+//namespace AutoVersionsDB.Core.IntegrationTests.AutoVersionsDbAPI_Tests
+//{
+//    [TestFixture]
+//    public class AutoVersionsDbAPI_DeliveryEnv_NotAollowMethods_Tests : AutoVersionsDbAPI_TestsBase
+//    {
 
 
-            //Act
-            ProcessResults processResults = AutoVersionsDbAPI.SetDBToSpecificState(projectConfig.Id, c_targetStateFile_FinalState, false, null);
+//        //[Test]
+//        //public void SetDBToSpecificState__Should_NotAllow([ValueSource("ProjectConfigItemArray_DeliveryEnv_ValidScripts")] ProjectConfigItemForTestBase projectConfig)
+//        //{
+//        //    //Arrange
+//        //    _mockProjectConfigsStorage.Setup(m => m.GetProjectConfigById(It.IsAny<string>())).Returns(projectConfig);
+
+//        //    string dbBackupFileFileFullPath = Path.Combine(FileSystemPathUtils.GetDllFolderFullPath(), "DbBackupsForTests", "AutoVersionsDB_MiddleState__incScript_2020-02-25.102_CreateLookupTable2.bak");
+//        //    restoreDB(projectConfig, dbBackupFileFileFullPath);
+
+//        //    NumOfConnections numOfOpenConnections_Before = getNumOfOpenConnection(projectConfig);
 
 
-            //Assert
-            assertNumOfOpenDbConnection(projectConfig, numOfOpenConnections_Before);
-            Assert.That(processResults.Trace.ContainErrorCode("DeliveryEnvironment"));
-        }
-
-        [Test]
-        public void Deploy__Should_NotAllow([ValueSource("ProjectConfigItemArray_DeliveryEnv_ValidScripts")] ProjectConfigItemForTestBase projectConfig)
-        {
-            //Arrange
-            _mockProjectConfigsStorage.Setup(m => m.GetProjectConfigById(It.IsAny<string>())).Returns(projectConfig);
-            _mockProjectConfigsStorage.Setup(m => m.IsIdExsit(It.IsAny<string>())).Returns(true);
-
-            string dbBackupFileFileFullPath = Path.Combine(FileSystemPathUtils.GetDllFolderFullPath(), "DbBackupsForTests", "AutoVersionsDB_FinalState_DeliveryEnv.bak");
-            restoreDB(projectConfig, dbBackupFileFileFullPath);
-
-            NumOfConnections numOfOpenConnections_Before = getNumOfOpenConnection(projectConfig);
-
-            //Act
-            ProcessResults processResults = AutoVersionsDbAPI.Deploy(projectConfig.Id, null);
+//        //    //Act
+//        //    ProcessResults processResults = AutoVersionsDbAPI.SetDBToSpecificState(projectConfig.Id, c_targetStateFile_FinalState, false, null);
 
 
-            //Assert
-            assertNumOfOpenDbConnection(projectConfig, numOfOpenConnections_Before);
-            Assert.That(processResults.Trace.ContainErrorCode("DeliveryEnvironment"));
-        }
+//        //    //Assert
+//        //    assertNumOfOpenDbConnection(projectConfig, numOfOpenConnections_Before);
+//        //    Assert.That(processResults.Trace.ContainErrorCode("DeliveryEnvironment"));
+//        //}
 
-        [Test]
-        public void RecreateDBFromScratch__Should_NotAllow([ValueSource("ProjectConfigItemArray_DeliveryEnv_ValidScripts")] ProjectConfigItemForTestBase projectConfig)
-        {
-            //Arrange
-            _mockProjectConfigsStorage.Setup(m => m.GetProjectConfigById(It.IsAny<string>())).Returns(projectConfig);
+//        //[Test]
+//        //public void Deploy__Should_NotAllow([ValueSource("ProjectConfigItemArray_DeliveryEnv_ValidScripts")] ProjectConfigItemForTestBase projectConfig)
+//        //{
+//        //    //Arrange
+//        //    _mockProjectConfigsStorage.Setup(m => m.GetProjectConfigById(It.IsAny<string>())).Returns(projectConfig);
+//        //    _mockProjectConfigsStorage.Setup(m => m.IsIdExsit(It.IsAny<string>())).Returns(true);
+
+//        //    string dbBackupFileFileFullPath = Path.Combine(FileSystemPathUtils.GetDllFolderFullPath(), "DbBackupsForTests", "AutoVersionsDB_FinalState_DeliveryEnv.bak");
+//        //    restoreDB(projectConfig, dbBackupFileFileFullPath);
+
+//        //    NumOfConnections numOfOpenConnections_Before = getNumOfOpenConnection(projectConfig);
+
+//        //    //Act
+//        //    ProcessResults processResults = AutoVersionsDbAPI.Deploy(projectConfig.Id, null);
+
+
+//        //    //Assert
+//        //    assertNumOfOpenDbConnection(projectConfig, numOfOpenConnections_Before);
+//        //    Assert.That(processResults.Trace.ContainErrorCode("DeliveryEnvironment"));
+//        //}
+
+//        //[Test]
+//        //public void RecreateDBFromScratch__Should_NotAllow([ValueSource("ProjectConfigItemArray_DeliveryEnv_ValidScripts")] ProjectConfigItemForTestBase projectConfig)
+//        //{
+//        //    //Arrange
+//        //    _mockProjectConfigsStorage.Setup(m => m.GetProjectConfigById(It.IsAny<string>())).Returns(projectConfig);
             
-            string dbBackupFileFileFullPath = Path.Combine(FileSystemPathUtils.GetDllFolderFullPath(), "DbBackupsForTests", "AutoVersionsDB_FinalState_DeliveryEnv.bak");
-            restoreDB(projectConfig, dbBackupFileFileFullPath);
+//        //    string dbBackupFileFileFullPath = Path.Combine(FileSystemPathUtils.GetDllFolderFullPath(), "DbBackupsForTests", "AutoVersionsDB_FinalState_DeliveryEnv.bak");
+//        //    restoreDB(projectConfig, dbBackupFileFileFullPath);
 
-            NumOfConnections numOfOpenConnections_Before = getNumOfOpenConnection(projectConfig);
+//        //    NumOfConnections numOfOpenConnections_Before = getNumOfOpenConnection(projectConfig);
 
-            //Act
-            ProcessResults processResults = AutoVersionsDbAPI.RecreateDBFromScratch(projectConfig.Id, null, null);
-
-
-            //Assert
-            assertNumOfOpenDbConnection(projectConfig, numOfOpenConnections_Before);
-            Assert.That(processResults.Trace.ContainErrorCode("DeliveryEnvironment"));
-        }
+//        //    //Act
+//        //    ProcessResults processResults = AutoVersionsDbAPI.RecreateDBFromScratch(projectConfig.Id, null, null);
 
 
+//        //    //Assert
+//        //    assertNumOfOpenDbConnection(projectConfig, numOfOpenConnections_Before);
+//        //    Assert.That(processResults.Trace.ContainErrorCode("DeliveryEnvironment"));
+//        //}
 
-    }
-}
+
+
+//    }
+//}
---
+++ src/AutoVersionsDB/AutoVersionsDB.Core.IntegrationTests/DBVersionsTests/TestDefinitions/DeliveryEnv_NotAollowMethods/DeliveryEnv_NotAllowMethods_Deploy_CLI.cs
@@ -0,0 +1,64 @@
+﻿using AutoVersionsDB;
+using AutoVersionsDB.Core;
+using AutoVersionsDB.Core.ConfigProjects;
+using AutoVersionsDB.Core.IntegrationTests;
+using AutoVersionsDB.Core.IntegrationTests.CLI;
+using AutoVersionsDB.Core.IntegrationTests.DB;
+using AutoVersionsDB.Core.IntegrationTests.DBVersionsTests;
+using AutoVersionsDB.Core.IntegrationTests.DBVersionsTests.TestDefinitions;
+using AutoVersionsDB.Core.IntegrationTests.DBVersionsTests.TestDefinitions.DeliveryEnv_NotAollowMethods;
+using AutoVersionsDB.Core.IntegrationTests.DBVersionsTests.TestDefinitions.DeliveryEnv_SyncDB;
+using AutoVersionsDB.Core.IntegrationTests.DBVersionsTests.TestDefinitions.DeliveryEnv_Validations;
+using AutoVersionsDB.Core.IntegrationTests.DBVersionsTests.TestDefinitions.DeliveryEnv_Virtual;
+using AutoVersionsDB.Core.IntegrationTests.ScriptFiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoVersionsDB.Core.IntegrationTests.DBVersionsTests.TestDefinitions.DeliveryEnv_NotAollowMethods
+{
+    public class DeliveryEnv_NotAllowMethods_Deploy_CLI : ITestDefinition
+    {
+
+        private readonly DeliveryEnv_NotAllowMethods_Deploy_API _deliveryEnv_NotAllowMethods_Deploy_API;
+
+        public DeliveryEnv_NotAllowMethods_Deploy_CLI(DeliveryEnv_NotAllowMethods_Deploy_API deliveryEnv_NotAllowMethods_Deploy_API)
+        {
+            _deliveryEnv_NotAllowMethods_Deploy_API = deliveryEnv_NotAllowMethods_Deploy_API;
+        }
+
+        public TestContext Arrange(ProjectConfigItem projectConfig, DBBackupFileType dbBackupFileType, ScriptFilesStateType scriptFilesStateType)
+        {
+            TestContext testContext = _deliveryEnv_NotAllowMethods_Deploy_API.Arrange(projectConfig, dbBackupFileType, scriptFilesStateType);
+
+            MockObjectsProvider.SetTestContextDataByMockCallbacks(testContext);
+
+            return testContext;
+        }
+
+
+        public void Act(TestContext testContext)
+        {
+            AutoVersionsDbAPI.CLIRun($"deploy -id={IntegrationTestsConsts.TestProjectId}");
+        }
+
+
+        public void Asserts(TestContext testContext)
+        {
+            _deliveryEnv_NotAllowMethods_Deploy_API.Asserts(testContext);
+
+            AssertTextByLines assertConsoleOutTextByLines = new AssertTextByLines(GetType().Name, "FinalConsoleOut", testContext.FinalConsoleOut);
+            assertConsoleOutTextByLines.AssertLineMessage(0, "> Run 'deploy' for 'IntegrationTestProject'", true);
+
+            AssertTextByLines assertErrorsTextByLines = new AssertTextByLines(GetType().Name, "ConsoleError", testContext.ConsoleError);
+            assertErrorsTextByLines.AssertLineMessage(0, "The process complete with errors:", true);
+            assertErrorsTextByLines.AssertLineMessage(1, "--------------------------------", true);
+            assertErrorsTextByLines.AssertLineMessage(2, "DeliveryEnvironment. Error: Could not run this command on Delivery Environment", false);
+            assertErrorsTextByLines.AssertLineMessage(3, "", true);
+            assertErrorsTextByLines.AssertLineMessage(4, "Could not run this command on Delivery Environment", true);
+
+
+        }
+
+    }
+}
