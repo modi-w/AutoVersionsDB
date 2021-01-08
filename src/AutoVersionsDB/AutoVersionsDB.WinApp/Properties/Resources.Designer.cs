39dc1316d6479c865bc7c70917b50a56f82f1410
modi-w <63314355+modi-w@users.noreply.github.com>
2021-01-08 21:54:12 +0200

    + change icon opacity + add banners to setup processes

--- src/AutoVersionsDB/AutoVersionsDB.WinApp/Properties/Resources.Designer.cs
+++ src/AutoVersionsDB/AutoVersionsDB.WinApp/Properties/Resources.Designer.cs
@@ -81,12 +81,12 @@ namespace AutoVersionsDB.WinApp.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
-        internal static System.Drawing.Bitmap AppIcon {
+        internal static System.Drawing.Icon AppIcon {
             get {
                 object obj = ResourceManager.GetObject("AppIcon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
+                return ((System.Drawing.Icon)(obj));
             }
         }
         
